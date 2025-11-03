7b0b2b6a540658fca9fe9e7a4a367d67818d33fb
archit <architjain1308@gmail.com>
2025-11-03 18:21:41 +0530

    Added input field for entering city name and basic  JavaScript handling

---
+++ Weather_App/js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('search-btn').addEventListener('click', function() {
+    const city = document.getElementById('city-input').value;
+    console.log('City entered:', city);
+});
